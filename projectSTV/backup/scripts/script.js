4f809e9e25ffed9798ab5556446a22d940d9d187
Vetten337 <79695259+Vetten337@users.noreply.github.com>
2025-11-14 19:11:49 -0600

    Add files via upload
    Added a hamburger menu for tablet and mobile view-ports
    Added JavaScript file
    Applied key-frame animations to pages within styles sheet
    
    Getting a validation warning with gallery.html  <section class="gallery-grid">  <!-- getting a validation warning here wanting me to use a div instead of section because i'm not using a heading. if i change to div, the heading changes my spacing and throws off the grid, i will keep the warning because it's a ton of work to rewrite --> <!-- figure classes for characters -->

---
+++ projectSTV/backup/scripts/script.js
@@ -0,0 +1,16 @@
+
+//Global variables
+var video = document.getElementById("example");
+var videoSource = document.getElementById("vid-src");
+var descriptionSource = document.getElementById("despsrc");
+
+//Hamburger menu function
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
+
